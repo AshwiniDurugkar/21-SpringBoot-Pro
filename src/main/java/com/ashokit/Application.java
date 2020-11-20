aff94b5a5ca133fa51f08dc048fbb02b38eb0244
AshwiniDurugkar <you@example.com>
2020-11-20 19:40:05 +0530

    HIS-200: Changes

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -8,6 +8,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
 	}
+        //HIS-200 Related Changes
+        public void doProcess()
+        {
+          //logic goes here  
+         }
 
 }
